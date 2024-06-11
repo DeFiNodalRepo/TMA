23abbe2a1a8f26713e87f65d66b5067e0831f39f
Cucuy 555 <cucuy555@gmail.com>
2024-06-11 14:15:59 +0300

    css menu update

--- src/components/MenuBar.tsx
+++ src/components/MenuBar.tsx
@@ -10,28 +10,28 @@ function MenuBar() {
     <div className='flex justify-center'>
       <div className="flex overflow-hidden bg-gray-900 border border-gray-700 divide-x divide-gray-700 rounded-lg rtl:flex-row-reverse">
         <NavLink to="/" className={({ isActive }) => isActive ? "bg-gray-700" : ""}>
-          <button className="flex flex-col items-center px-4 py-2 mx-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
+          <button className="flex flex-col items-center px-4 py-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
             <MdDashboard size={25} />
             <span>Dashboard</span>
           </button>
         </NavLink>
 
         <NavLink to="/vaults" className={({ isActive }) => isActive ? "bg-gray-700" : ""}>
-          <button className="flex flex-col items-center px-4 py-2 mx-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
+          <button className="flex flex-col items-center px-4 py-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
             <FaVault size={25} />
             <span>Vaults</span>
           </button>
         </NavLink>
 
         <NavLink to='/missions' className={({ isActive }) => isActive ? "bg-gray-700" : ""}>
-          <button className="flex flex-col items-center px-4 py-2 mx-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
+          <button className="flex flex-col items-center px-4 py-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
             <FiTarget size={25} />
             <span>Missions</span>
           </button>
         </NavLink>
 
         <NavLink to="/referrals" className={({ isActive }) => isActive ? "bg-gray-700" : ""}>
-          <button className="flex flex-col items-center px-4 py-2 mx-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
+          <button className="flex flex-col items-center px-4 py-2 text-xs font-medium text-gray-300 transition-colors duration-200 gap-x-3">
             <GiThreeFriends size={25} />
             <span>Friends</span>
           </button>
