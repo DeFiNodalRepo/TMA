6dddfe28041844390f50922d00c137217b152ff0
Cucuy 555 <cucuy555@gmail.com>
2024-06-12 16:52:23 +0300

    card component

--- src/pages/Vaults.tsx
+++ src/pages/Vaults.tsx
@@ -1,10 +1,12 @@
 
+import VaultCards from '../components/VaultCards'
 import DefaultLayout from '../layout/DefaultLayout'
 
 function Vaults() {
   return (
     <DefaultLayout >
       <div className='text-white'>Vaults</div>
+      <VaultCards />
     </DefaultLayout>
   )
 }
