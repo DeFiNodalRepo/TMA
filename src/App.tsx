eff0d9e1cc0a3368623b0fb00306b72515280363
Cucuy 555 <cucuy555@gmail.com>
2024-07-26 15:55:26 +0300

    share link created

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Loader from "./components/Loader";
 import { useEffect, useState } from "react";
 import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-// import { initInitData } from '@telegram-apps/sdk';
 import { queryClient } from '../src/react-query/queryClient'
 import { getAuth, getConfData, getInitData } from "./api/apiCalls";
 import { AppContext, InitUserContext, SyncDataProvider } from "./state-management/context";
@@ -19,14 +18,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState('');
 
-  // const tgData = window.Telegram.WebApp
-  // const initSessionData = tgData.initData
+  const tgData = window.Telegram.WebApp
+  const initSessionData = tgData.initData
 
   // console.log("initSessionData", initSessionData)
 
-  // const initialData = initInitData();
-
-  // console.log("initialData sdk", initialData)
 
   useEffect(() => {
     setTimeout(() => setLoading(false), 1000);
@@ -39,7 +35,7 @@ function App() {
         setIsLoading(true);
   
         // Fetch API Token
-        const apiTokenData = await getAuth('user=%7B%22id%22%3A6915997019%2C%22first_name%22%3A%22LePezoun%22%2C%22last_name%22%3A%22%22%2C%22language_code%22%3A%22en%22%2C%22allows_write_to_pm%22%3Atrue%7D&chat_instance=447728990447128365&chat_type=private&auth_date=1720541535&hash=5232c3496c02e894d81368bf23565bcf1e2a7d2ef4c3f5a42a2b4e761a909113');
+        const apiTokenData = await getAuth(initSessionData);
         
           setApiToken(apiTokenData);
           const token = await apiTokenData?.body
@@ -58,9 +54,7 @@ function App() {
     };
   
     fetchData();
-  }, []);
-  // Uncomment on production
-  // }, [initSessionData]);
+  }, [initSessionData]);
 
   if (isLoading) {
     return <Loader />;
--- src/pages/Referral.tsx
+++ src/pages/Referral.tsx
@@ -4,6 +4,7 @@ import { FaEnvelopeOpen } from "react-icons/fa";
 import { AppContext } from '../state-management/context';
 import { useSyncData } from '../react-query/useSyncData';
 import Loader from '../components/Loader';
+import { initInitData, initUtils  } from '@telegram-apps/sdk';
 
 function Referrals() {
 
@@ -11,6 +12,16 @@ function Referrals() {
   const token = authData?.body
 
   const {data, isError, isLoading} = useSyncData(token)
+
+  
+  const initialData = initInitData();
+  const utils = initUtils()
+
+  console.log("initialData sdk", initialData?.user?.id)
+
+  const inviteLink = `https://t.me/DeFiNodal_bot/definodal_miniapp/start?startapp=${initialData?.user?.id}`
+
+  // https://t.me/DeFiNodal_bot/definodal_miniapp/start?startapp=3A6915997019
 
   if(isLoading){
     <DefaultLayout>
@@ -29,18 +40,22 @@ function Referrals() {
 
   const handleCopyInviteLink = () => {
     console.log("handleCopyInviteLink")
+    utils.shareURL(inviteLink)
   }
 
   return (
     <DefaultLayout >
-      <div className='text-white'>Referrals</div>
-      <div className="flex flex-wrap items-center justify-center gap-5">
+      <div>Referrals</div>
+      <div className="flex items-center justify-center space-x-6">
 
-        <button type="button" className="inline-flex items-center gap-1.5 rounded-lg border border-green-500 bg-green-500 px-5 py-2.5 text-center text-xl font-medium text-white shadow-sm transition-all hover:border-green-700 hover:bg-green-700 focus:ring focus:ring-green-200 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300" onClick={handleCopyInviteLink}>
-
-          Invite Link
+        <button type="button" className="text-md inline-flex items-center gap-1.5 rounded-lg border border-green-500 bg-green-500 px-5 py-2.5 text-center font-medium text-white shadow-sm transition-all hover:border-green-700 hover:bg-green-700 focus:ring focus:ring-green-200 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300" onClick={handleCopyInviteLink}>
+          Share Link
           <FaEnvelopeOpen size={20} className='ml-2'/>
         </button>     
+          <button type="button" className="text-md inline-flex items-center gap-1.5 rounded-lg border border-green-500 bg-green-500 px-5 py-2.5 text-center font-medium text-white shadow-sm transition-all hover:border-green-700 hover:bg-green-700 focus:ring focus:ring-green-200 disabled:cursor-not-allowed disabled:border-green-300 disabled:bg-green-300" onClick={handleCopyInviteLink}>
+          Copy Link
+          <FaEnvelopeOpen size={20} className='ml-2'/>
+        </button>  
       </div>
     </DefaultLayout>
   )
