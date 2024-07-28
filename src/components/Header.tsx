9e1572e6c2800ee33b48bf052b6d4c65cc9fa062
Cucuy555 <cucuycy555@gmail.com>
2024-07-28 21:08:44 +0300

    fetch on mount useSync

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <div className='flex justify-center mt-4 mb-4'>
-      <img src="src/assets/DefiNodal_Logo-light-modern-removebg.png" alt='DefiNodal Onboarding' width={30} height={30} />
+      <img src="https://raw.githubusercontent.com/DeFiNodal/TMA/main/img/DefiNodal_Logo-light-modern-removebg.png" alt='DefiNodal Onboarding' width={30} height={30} />
 
       <div className='ml-2 text-2xl font-semibold text-gray-300'>DefiNodal Onboarding</div>
 
